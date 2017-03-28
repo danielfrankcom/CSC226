91efeec4d5ca905872ca701830f7284dd5f2128f
Daniel Frankcom <danielfrankcom@gmail.com>
2017-03-27 20:31:15 -0700

    Comments

--- As4p/KMP.java
+++ As4p/KMP.java
@@ -18,49 +18,71 @@ public class  KMP{
 
     public static int search(String txt){
 
+        // declarations
         int numCharsText = txt.length();
         int numCharsPattern = dfs[0].length;
         int pointer = 0;
+
+        // loop through search text
         for(int i = 0; i < numCharsText ; i++){
+
+            // if char at current index is next in pattern
             if((int) txt.charAt(i) == dfs[0][pointer]){
-                pointer ++;
+                pointer ++;  // increase location to look in pattern
             }else{
+                // set location to look in pattern as dictated by DFS
                 pointer = dfs[1][pointer];
             }
+
+            // if entire pattern has been found
             if(pointer == numCharsPattern){
-                return i - numCharsPattern + 1;
+                return i - numCharsPattern + 1;  // return index of beginning of pattern
             }
+
         }
 
-        return numCharsText;
+        return numCharsText;  // return length of text as pattern not found
 
     }
 
     public KMP(String pattern){
 
+        // declarations
         int numChars = pattern.length();
         int[] characters = new int[numChars];
+        int[] locations = new int[numChars];
+
+        // populate array with char representations
         for(int i = 0; i < numChars; i++){
-            characters[i] = (int) pattern.charAt(i);
+            characters[i] = (int) pattern.charAt(i);  // integer representation of each char
         }
 
-        int[] locations = new int[numChars];
+        // populate array with locations if char does not match
         for(int i = 0; i < numChars; i++){
+
+            // declare iterating string, starting at chars up until current index
+            // start with highest value match, so as to keep location pointer high
             String tempStr = pattern.substring(0, i);
             int tempStrLen = i - 1;
-            while(tempStrLen > 0){
+
+            while(tempStrLen > 0){  // while potential match in previous string
+
+                // check iterating string against pattern of same length
+                // starting immediately before the current index
                 if(pattern.substring(i - tempStrLen, i).equals(tempStr)){
-                    locations[i] = tempStrLen;
+                    locations[i] = tempStrLen;  // store location pointer if pattern found
                 }
 
+                // decrease string in size until only the beginning chars
+                // in the pattern are reached
                 tempStrLen -= 1;
                 tempStr = tempStr.substring(0, tempStrLen);
+
             }
 
         }
-        //System.out.println(Arrays.toString(locations));
 
-        dfs = new int[][]{characters, locations};
+        dfs = new int[][]{characters, locations};  // set public dfs for use
 
     }
 
